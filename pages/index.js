18b394f1167319ac8e04d31b56da9bf3c3511faa
Jason Stinson <jsfeb26@gmail.com>
2017-06-24 11:23:04 -0400

    update gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,14 @@
-import Link from 'next/link'
-import fetch from 'isomorphic-fetch'
+import Link from 'next/link';
 
-const Index = (props) => (
+import Head from '../shared/head';
+
+export default () => (
   <div>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.photos.map((show) => (
-        <li key={show.id}>
-          <a>{show.name}</a>
-        </li>
-      ))}
-    </ul>
+    <Head />
+    <div>
+      <Link href="/search">Search</Link>
+      <Link href="/favorites">Favorites</Link>
+      Main
+    </div>
   </div>
-)
-
-Index.getInitialProps = async function() {
-  const res = await fetch(`https://api.500px.com/v1/photos/search?term=golden hour&tags=goldenhour,golden hour,sunset,sunrise&geo=40.7162735,-73.9607704,13.29km&image_size=21&sort=highest_rating&consumer_key=FBd1TAlMzdqkwKoQLJsqHYJh81OMRhY5o44e0NvU`);
-  const data = await res.json();
-  return {
-    photos: data.photos
-  };
-}
-
-export default Index
+);
