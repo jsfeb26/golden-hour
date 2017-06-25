337226ffa5bb2f2fb7617d02ff7588f6cfdad24c
Jason Stinson <jsfeb26@gmail.com>
2017-06-25 19:48:47 -0400

    lower amount of results

--- endpoints.js
+++ endpoints.js
@@ -1,4 +1,4 @@
 export default {
-  "GET_PHOTOS": (GEO, CONSUMER_KEY) => `https://api.500px.com/v1/photos/search?term=golden hour&tags=goldenhour,golden hour,sunset,sunrise&geo=${GEO}&image_size=600,4&sort=highest_rating&rpp=50&consumer_key=${CONSUMER_KEY}`,
+  "GET_PHOTOS": (GEO, CONSUMER_KEY) => `https://api.500px.com/v1/photos/search?term=golden hour&tags=goldenhour,golden hour,sunset,sunrise&geo=${GEO}&image_size=600,4&sort=highest_rating&rpp=30&consumer_key=${CONSUMER_KEY}`,
   "GEOCODE": (ADDRESS) => `https://maps.googleapis.com/maps/api/geocode/json?address=${ADDRESS}`
 };
